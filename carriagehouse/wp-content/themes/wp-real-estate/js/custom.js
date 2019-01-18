82a165f6142b8723e4aa1031ebe75ddb0fce7586
plsms <psomoso@gmail.com>
2019-01-18 02:13:46 -0600

    -

---
+++ carriagehouse/wp-content/themes/wp-real-estate/js/custom.js
@@ -0,0 +1,17 @@
+jQuery(document).ready( function() {
+	jQuery('#searchicon').click(function() {
+		jQuery('#jumbosearch').fadeIn();
+		jQuery('#jumbosearch input').focus();
+	});
+	jQuery('#jumbosearch .closeicon').click(function() {
+		jQuery('#jumbosearch').fadeOut();
+	});
+	jQuery('body').keydown(function(e){
+	    
+	    if(e.keyCode == 27){
+	        jQuery('#jumbosearch').fadeOut();
+	    }
+	});
+
+	
+});
